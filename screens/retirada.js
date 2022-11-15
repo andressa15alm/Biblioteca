2995a8a5ced09e7d7d71a1f6073ff66f16c29977
andressa15alm <88633758+andressa15alm@users.noreply.github.com>
2022-11-15 14:50:07 -0300

    Update retirada.js

--- screens/retirada.js
+++ screens/retirada.js
@@ -1,11 +1,58 @@
 import React, { Component } from "react";
-import { View,Text,StyleSheet } from "react-native";
-
+import { View,Text,StyleSheet,TouchableOpacity } from "react-native";
+import { BarCodeScanner } from "expo-barcode-scanner";
+import * as Permissions from "expo-permissions"
 export default class RetiradaScreen extends Component{
+
+    constructor(props){
+        super(props);
+        this.state={
+            domState:"normal",
+            hasCameraPermissions:null,
+            scanned:false,
+            scannedData:""
+        }
+    }
+
+
+    getCameraPermissions=async domState=>{
+        const {status}=await Permissions.askAsync(Permissions.CAMERA);
+
+        this.setState({
+           hasCameraPermissions:status=== "grated",
+            domState:domState,
+            scanned:false
+        });
+    };
+
+    handleBarCodeScanned=async({type,data})=>{
+        this.setState({
+            scannedData:data,
+            domState:"normal",
+            scanned:true
+        });
+    }
+
     render(){
+
+        const{domState,hasCameraPermissions,scannedData,scanned}=this.state;
+            if(domState==="scanner"){
+                return(<BarCodeScanner
+                    onBarCodeScanned={scanned ? undefined : this.handleBarCodeScanned}
+                    style={StyleSheet.absoluteFillObject}/>
+                )
+            }
         return(
             <View style={styles.container}>
-                <Text style={styles.texto}>Tela de retirada</Text>
+                <Text style={styles.textms}>
+                    {hasCameraPermissions? scannedData:"Solicitar Permissão para a câmera"}
+                </Text>
+                <TouchableOpacity style={styles.btqr}
+                onPress={()=>this.getCameraPermissions("scanner")}
+                >
+                <Text style={styles.texto}>QR Code</Text>
+                </TouchableOpacity>
+                   
             </View>
         );
     }
@@ -21,5 +68,13 @@ const styles= StyleSheet.create({
     texto:{
         color:"#ffffff",
         fontSize:30,
-    }
+    },
+    btqr:{
+        width:"43%",
+        height:55,
+        justifyContent:"center",
+        alignItems:"center",
+        backgroundColor:"#f48d20",
+        borderRadius:15,
+    },
 })
